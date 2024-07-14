1503bd17a9085314d6be21f467ce515f02ed8091
Jakub Burkiewicz <jakub.burkiewicz@conx.eu>
2024-07-14 12:21:34 +0200

    refactor: Simplify App.js by removing unnecessary code and updating dependencies

--- App.js
+++ App.js
@@ -3,15 +3,12 @@ import { NavigationContainer } from '@react-navigation/native'
 import ScreenSwitcher from './ScreenSwitcher'
 import { ProfileProvider } from './contexts/ProfileContext'
 
-const App = () => {
-
-    return (
-        <ProfileProvider>
-            <NavigationContainer>
-                <ScreenSwitcher />
-            </NavigationContainer>
-        </ProfileProvider>
-    )
-}
+const App = () => (
+    <ProfileProvider>
+        <NavigationContainer>
+            <ScreenSwitcher />
+        </NavigationContainer>
+    </ProfileProvider>
+)
 
 export default App
